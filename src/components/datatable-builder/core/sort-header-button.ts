998ccb93b82b2c81245aad254df856c412294ae0
softsurgery <houssemeddine.chibouni@zedneycreative.com>
2026-06-11 16:44:54 +0100

    feat(datatable): enhance sort header button with dropdown menu for sorting options and update table header styling

--- src/components/datatable-builder/core/sort-header-button.ts
+++ src/components/datatable-builder/core/sort-header-button.ts
@@ -1,33 +1,85 @@
 import { Component, computed, input } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideArrowUpDown } from '@ng-icons/lucide';
+import {
+  lucideArrowDown,
+  lucideArrowUp,
+  lucideChevronsUpDown,
+} from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmDropdownMenuImports } from '@spartan-ng/helm/dropdown-menu';
 import { HlmIconImports } from '@spartan-ng/helm/icon';
-import { type HeaderContext, injectFlexRenderContext } from '@tanstack/angular-table';
+import {
+  HeaderContext,
+  injectFlexRenderContext,
+} from '@tanstack/angular-table';
 
 @Component({
-  imports: [HlmButtonImports, NgIcon, HlmIconImports],
-  providers: [provideIcons({ lucideArrowUpDown })],
+  selector: 'app-table-head-sort-button',
+  standalone: true,
+  imports: [
+    HlmButtonImports,
+    HlmDropdownMenuImports,
+    HlmIconImports,
+    NgIcon,
+  ],
+  providers: [
+    provideIcons({
+      lucideChevronsUpDown,
+      lucideArrowUp,
+      lucideArrowDown,
+    }),
+  ],
   template: `
     <button
       hlmBtn
       size="sm"
       variant="ghost"
-      (click)="filterClick()"
+      [hlmDropdownMenuTrigger]="menu"
       [class.capitalize]="header() === ''"
     >
-      {{ _header() }}
-      <ng-icon hlm size="sm" name="lucideArrowUpDown" />
+      <span class="font-bold">
+        {{ _header() }}
+      </span>
+
+      <ng-icon
+        hlm
+        size="sm"
+        name="lucideChevronsUpDown"
+      />
     </button>
+
+    <ng-template #menu>
+      <hlm-dropdown-menu class="w-40">
+        <button hlmDropdownMenuItem (click)="sortAsc()">
+          <ng-icon name="lucideArrowUp" />
+          Ascending
+        </button>
+
+        <button hlmDropdownMenuItem (click)="sortDesc()">
+          <ng-icon name="lucideArrowDown" />
+          Descending
+        </button>
+      </hlm-dropdown-menu>
+    </ng-template>
   `,
 })
 export class TableHeadSortButton<T> {
-  protected readonly _context = injectFlexRenderContext<HeaderContext<T, unknown>>();
-  protected filterClick() {
-    this._context.column.toggleSorting(this._context.column.getIsSorted() === 'asc');
-  }
+  protected readonly _context =
+    injectFlexRenderContext<HeaderContext<T, unknown>>();
+
   public readonly header = input('');
-  protected readonly _header = computed(() => {
-    return this.header() === '' ? this._context.column.id : this.header();
-  });
+
+  protected readonly _header = computed(() =>
+    this.header() === ''
+      ? this._context.column.id
+      : this.header()
+  );
+
+  protected sortAsc() {
+    this._context.column.toggleSorting(false);
+  }
+
+  protected sortDesc() {
+    this._context.column.toggleSorting(true);
+  }
 }
